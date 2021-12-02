3e64cd239c02eb4560cef06fd6ce214fc0071580
Iker Lissarrague <ikerlb95@gmail.com>
2021-12-02 02:29:48 -0600

    slight improvement to day 2

--- 2/sol.clj
+++ 2/sol.clj
@@ -11,27 +11,21 @@
 (def lines
   (map parse-line (get-lines "./input.txt")))
 
-(defn step1 [x y cmd i]
+(defn step-part1 [x y cmd i]
   (cond
     (= cmd "forward") [(+ x i) y]
     (= cmd "up") [x (- y i)]
     :else [x (+ y i)]))
 
-(defn step2 [x y aim cmd i]
+(defn step-part2 [x y aim cmd i]
   (cond
     (= cmd "forward") [(+ x i) (- y (* aim i)) aim]
     (= cmd "up") [x y (+ aim i)]
     :else [x y (- aim i)]))
-    
-(defn part1 [cmds sx sy]
-  (reduce #(let [[x y] %1
-                 [cmd i] %2]
-             (step1 x y cmd i)) [sx sy] cmds))
 
-(defn part2 [cmds sx sy sa]
-  (reduce #(let [[x y a] %1
-                 [cmd i] %2]
-             (step2 x y a cmd i)) [sx sy sa] cmds))
+;; list of commands, init vector, step function to reduce
+(defn solve [cmds init step-fn]
+  (reduce #(apply step-fn (into %1 %2)) init cmds))
 
-(apply * (part1 lines 0 0))
-(apply * (take 2 (part2 lines 0 0 0)))
+(apply * (solve lines [0 0] step-part1))
+(apply * (take 2 (solve lines [0 0 0] step-part2)))
