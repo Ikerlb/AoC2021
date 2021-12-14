90538cfd716243610f47039378575f6e35027ee5
Iker Lissarrague <ikerlb95@gmail.com>
2021-12-13 18:48:13 -0600

    slight change for day 12

--- 12/sol.py
+++ 12/sol.py
@@ -10,38 +10,39 @@ with open("input.txt", "r") as f:
 
 # here's to hoping there
 # are no uppercase loops
-def dfs1(g, node, visited, path, res):
+def dfs1(g, node, visited):
     if node == "end":
-        res.append(path + ["end"])        
-        return
+        return 1
+    res = 0
     for nn in g[node]:    
         if nn.isupper() or nn not in visited:
             visited.add(nn) 
-            dfs1(g, nn, visited, path + [node], res)
+            res += dfs1(g, nn, visited)
             visited.discard(nn)    
+    return res
 
 # again hoping there 
 # are no uppercase loops
-def dfs2(g, node, visited, path, res, used):
+def dfs2(g, node, visited, used):
     if node == "end":        
-        res.append(path + ["end"])    
-        return
+        return 1
+    res = 0
     for nn in g[node]:
         if nn.isupper():    
-            dfs2(g, nn, visited, path + [node], res, used)    
+            res += dfs2(g, nn, visited, used)    
         elif nn in visited and nn != "start" and not used:
-            dfs2(g, nn, visited, path + [node], res, True)
+            res += dfs2(g, nn, visited, True)
         elif nn not in visited:
             visited.add(nn)
-            dfs2(g, nn, visited, path + [node], res, used)
+            res += dfs2(g, nn, visited, used)
             visited.discard(nn)
+    return res
 
 def part1(g):
-    res = []        
-    dfs1(g, "start", {"start"}, [], res)
-    return len(res)
+    return dfs1(g, "start", {"start"})
 
 def part2(g):
-    res = []    
-    dfs2(g, "start", {"start"}, [], res, False)
-    return len(res)
+    return dfs2(g, "start", {"start"}, False)
+
+print(part1(g))
+print(part2(g))
